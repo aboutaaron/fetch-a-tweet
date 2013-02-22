c37360956d5a36574bf55920c4e7808267b18a4f
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-21 19:01:06 -0800

    update rails. Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FetchATweet::Application.config.secret_token = '7e85a735cebb407d630a39e68a86f7441a3999158efa9c7108e60d9619953a1738802f9ed9bc3a1c58292ea73206ce5b476d1e4bc9900a25b68d30611d42116f'
