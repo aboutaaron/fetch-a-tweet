7e7effb2fa0d3468d5916a57c3d2d852b34c7433
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-23 15:54:15 -0800

    added new controllers and model validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :content, :tweet_id, :user
+
+  validates :content, :tweet_id, :user, :presence => true
+  validates :tweet_id, :uniqueness => true
+  validates :tweet_id, :numericality => true
 end
