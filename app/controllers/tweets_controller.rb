ce962949d8ca4617c043759e6fc896b5f73947c0
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-23 15:22:21 -0800

    created basic scaffold for tweet controller and view. Also added bootstrap

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,13 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @tweets }
+    end
+  end
+
+  def new
+  end
+end
