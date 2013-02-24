9a0bdc715aea6975d2b26d1e53f0755d1af7137d
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-23 21:48:52 -0800

    scaffolding for a basic app

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,10 +10,28 @@ class TweetsController < ApplicationController
 
   def new
     @tweet = Tweet.new
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html # show.html.erb
       format.json { render json: @tweet }
+    end
+  end
+
+  def create
+    @tweet = Tweet.new(params[:tweet])
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.json { render json: @tweet, status: :created, location: @tweet }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
     end
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,6 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :content, :tweet_id, :user
 
-  validates :content, :tweet_id, :user, :presence => true
+  validates :tweet_id, :presence => true
   validates :tweet_id, :uniqueness => true
-  validates :tweet_id, :numericality => true
 end
