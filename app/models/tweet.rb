70c8d8aa3a34b90e12603b818cb19904f459e7b4
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-24 11:50:48 -0800

    reversed the order of the index page and still working out validsation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,8 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :content, :tweet_id, :user
 
-  validates :tweet_id, :presence => true
+  validates :tweet_id, :user, :content, :presence => true
   validates :tweet_id, :uniqueness => true
   validates :tweet_id, :length  => { :is => 18 }
+  validates :tweet_id, :numericality => { :only_integer => true }
 end
