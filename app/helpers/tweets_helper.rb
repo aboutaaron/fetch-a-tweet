4ac623e48a0433a6180055c092ab44e19b7d4ecd
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-23 21:48:52 -0800

    scaffolding for a basic app

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,9 @@
 module TweetsHelper
+    def retrieve_twitter_user(id)
+        user = Twitter.user(id)
+    end
+
+    def retrieve_twitter_status(id)
+        status = Twitter.status(id).text
+    end
 end
