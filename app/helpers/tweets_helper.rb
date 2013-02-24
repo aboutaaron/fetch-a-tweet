473b3261f07161eb4b69f8d5f6b59fdd041926e3
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-24 00:56:05 -0800

    finished it

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,9 +1,2 @@
 module TweetsHelper
-    def retrieve_twitter_user(id)
-        user = Twitter.user(id)
-    end
-
-    def retrieve_twitter_status(id)
-        status = Twitter.status(id).text
-    end
 end
