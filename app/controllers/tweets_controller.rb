5e55552a8d89b165bdf79a7f3c2dda73a141da5c
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-25 20:22:05 -0800

    Added error handle in controller for the app

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,17 +12,21 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new
-    @tweet.user = Twitter.status(params[:tweet][:tweet_id]).from_user
-    @tweet.tweet_id = params[:tweet][:tweet_id]
-    @tweet.content = Twitter.status(params[:tweet][:tweet_id]).text
-
-    respond_to do |format|
-      if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-      else
-        format.html { render action: "new" }
+    begin
+      @tweet = Tweet.new
+      @tweet.user = Twitter.status(params[:tweet][:tweet_id]).from_user
+      @tweet.tweet_id = params[:tweet][:tweet_id]
+      @tweet.content = Twitter.status(params[:tweet][:tweet_id]).text
+      respond_to do |format|
+        if @tweet.save
+          format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        else
+          format.html { render action: "new" }
+        end
       end
+    rescue => e
+      @error = "Sorry that tweet doesn't exist. Try again?"
+      render :action => 'new'
     end
   end
 end
