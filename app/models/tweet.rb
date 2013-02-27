9c7b73a4c3e42a1dc4c7518170292389df055fcb
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-27 10:44:33 -0800

    removed length validation so I can accept jack's first tweet.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,6 +3,5 @@ class Tweet < ActiveRecord::Base
 
   validates :tweet_id, :presence => true
   validates :tweet_id, :uniqueness => true
-  validates :tweet_id, :length  => { :is => 18 }
   validates :tweet_id, :numericality => { :only_integer => true }
 end
