9ff4f90070b3aa634040a86cd90eddeb2be6ee9a
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-26 22:06:24 -0800

    removed avater helper. page load is way too slow.

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -6,8 +6,4 @@ module TweetsHelper
     def hyperlink_the_tings(str)
         auto_link(str).html_safe
     end
-
-    def grab_user_avatar(str)
-        Twitter.status(str)["profile_image_url"]
-    end
 end
