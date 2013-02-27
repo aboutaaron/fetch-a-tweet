f0aedaa0c38bb4ce13130705ac25690b5205c2f5
Tim Haines <tmhaines@gmail.com>
2013-02-26 22:48:06 -0800

    Assign a fetched tweet to tweet_from_twitter, and use its methods to access properties

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,13 +12,15 @@ class TweetsController < ApplicationController
   end
 
   def create
-    begin
-      t = Twitter.status(params[:tweet][:tweet_id]).attrs
-      @tweet = Tweet.new
-      @tweet.user = t[:user][:name]
-      @tweet.tweet_id = t[:id]
-      @tweet.content = t[:text]
+    tweet_from_twitter = get_tweet_from_twitter(params[:tweet][:tweet_id].to_i)
+    @tweet = Tweet.new
+
+    if tweet_from_twitter
+      @tweet.user = tweet_from_twitter.user.name
+      @tweet.tweet_id = tweet_from_twitter.id
+      @tweet.content = tweet_from_twitter.text
       # @image = t[:profile_image_url]
+
       respond_to do |format|
         if @tweet.save
           format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
@@ -26,9 +28,18 @@ class TweetsController < ApplicationController
           format.html { render action: "new" }
         end
       end
-    rescue => e
+
+    else
       @error = "Sorry that tweet doesn't exist. Try again?"
       render :action => 'new'
     end
   end
+
+  protected
+    def get_tweet_from_twitter(tweet_id)
+      return Twitter.status(tweet_id)
+    rescue
+      # Could log error or do something else here.
+      return nil
+    end
 end
